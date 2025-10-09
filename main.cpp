ff9bbc08f079ccca53bc56bfea33590431088203
ryuyoshi223-coder <ryuyoshi223@gmail.com>
2025-10-09 13:44:20 +0900

    test_a

--- main.cpp
+++ main.cpp
@@ -124,6 +124,8 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	//sdfghjk
 
+	//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
 
